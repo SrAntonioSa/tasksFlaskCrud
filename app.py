c5ffc6d717a16037cda1de71ad05373eced498ce
SrAntonioSa <antonio.m.sa@hotmail.com>
2025-04-14 12:55:55 +0200

    updating the create task function

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
-from flask import Flask
-
+from flask import Flask, request,jsonify
+from models.task import Task
 
 
 app=Flask(__name__)
 
 
-# criando rota
+#CRUD
+# Create - Read - Update - Delete
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
 
-@app.route("/about")
-def about():
-    return "Pagina sobre"
+
+tasks = []
+task_id_control = 1
+
+
+@app.route('/tasks',methods = ['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id= task_id_control, title=data.get('title'), description=data.get("description", ""))
+    task_id_control+=1
+    tasks.append(new_task)
+    print(data)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
 
 
 if __name__=="__main__":
